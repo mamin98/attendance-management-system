6d8ba7755d78565fcea2dd5ccabb3d239a88bc59
mohamed.amin <mohamedamin1421@gmail.com>
2026-05-16 18:48:51 +0300

    feat: implement jwt

---
+++ AttendanceSystem.Infrastructure/Security/JwtTokenGenerator.cs
@@ -0,0 +1,47 @@
+using AttendanceSystem.Application;
+using AttendanceSystem.Domain;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AttendanceSystem.Infrastructure;
+
+public class JwtTokenGenerator(
+    IConfiguration configuration)
+    : IJwtTokenGenerator
+{
+    readonly IConfiguration _configuration = configuration;
+
+    public string GenerateToken(Employee employee)
+    {
+        IConfiguration jwtSettings = _configuration.GetSection("Jwt");
+
+        SymmetricSecurityKey key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwtSettings["Key"]!));
+
+        SigningCredentials credentials = new SigningCredentials(
+            key,
+            SecurityAlgorithms.HmacSha256);
+
+        Claim[] claims =
+        [
+            new Claim(JwtRegisteredClaimNames.Sub, employee.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, employee.Email),
+            new Claim(ClaimTypes.Role, employee.Role.ToString()),
+            new Claim(ClaimTypes.Name, employee.NameEnglish)
+        ];
+
+        JwtSecurityToken token = new JwtSecurityToken(
+            issuer: jwtSettings["Issuer"],
+            audience: jwtSettings["Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(
+                Convert.ToDouble(jwtSettings["ExpireMinutes"])),
+            signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler()
+            .WriteToken(token);
+    }
+}
