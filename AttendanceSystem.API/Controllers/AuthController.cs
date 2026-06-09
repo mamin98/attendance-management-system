85da573557a47d9d9a3a6bca0ae30c3ea048c9a1
mohamed.amin <mohamedamin1421@gmail.com>
2026-06-09 16:06:43 +0300

    feat: add change password api

--- AttendanceSystem.API/Controllers/AuthController.cs
+++ AttendanceSystem.API/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
+using System.Security.Claims;
 using AttendanceSystem.Application;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AttendanceSystem.API;
@@ -21,4 +23,17 @@ public class AuthController(IAuthService authService)
             ApiResponse<LoginResponseDto>
             .SuccessResponse(result));
     }
+
+    [Authorize]
+    [HttpPost("change-password")]
+    public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordDto dto)
+    {
+        Guid employeeId = Guid.Parse(
+            User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+        await _authService.ChangePasswordAsync(employeeId, dto);
+
+        return Ok(ApiResponse<string>.SuccessResponse(null, "Password changed successfully"));
+    }
+
 }
