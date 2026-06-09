c4d67a04effde4a802599ab68718ae494d40d6ea
mohamed.amin <mohamedamin1421@gmail.com>
2026-06-09 15:58:15 +0300

    feat: add login request validator

---
+++ AttendanceSystem.Application/Validators/Cases/Auth/LoginRequestValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+
+namespace AttendanceSystem.Application;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequestDto>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters");
+    }
+}
