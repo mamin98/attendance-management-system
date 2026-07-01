e27e565f5d255af82b31c3212fdc8a1cffa43128
mohamed.amin <mohamedamin1421@gmail.com>
2026-07-01 16:38:52 +0300

    feat: setup serilog

--- AttendanceSystem.API/Program.cs
+++ AttendanceSystem.API/Program.cs
@@ -1,26 +1,46 @@
-using AttendanceSystem.Infrastructure;
 using AttendanceSystem.Application;
+using AttendanceSystem.Infrastructure;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 
-namespace AttendanceSystem.API
+namespace AttendanceSystem.API;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+            .MinimumLevel.Information()
+            .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.File(
+                "Logs/log-.txt",
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 14)
+            .CreateLogger();
+
+        try
         {
+            Log.Information("Starting AttendanceSystem API");
+
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Host.UseSerilog();
+
             builder.Services.AddInfrastructure(builder.Configuration);
             builder.Services.AddApplication();
 
             builder.Services
-            .AddFluentValidationAutoValidation()
-            .AddFluentValidationClientsideAdapters();
+                .AddFluentValidationAutoValidation()
+                .AddFluentValidationClientsideAdapters();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(options =>
             {
@@ -54,41 +74,46 @@ namespace AttendanceSystem.API
                         }
                     });
             });
-    
-    
+
             var app = builder.Build();
 
             using (var scope = app.Services.CreateScope())
             {
-                AttendanceDbContext dbContext = scope.ServiceProvider
+                var dbContext = scope.ServiceProvider
                     .GetRequiredService<AttendanceDbContext>();
 
                 await dbContext.Database.MigrateAsync();
 
-                DataSeeder seeder = scope.ServiceProvider
+                var seeder = scope.ServiceProvider
                     .GetRequiredService<DataSeeder>();
 
                 await seeder.SeedAsync();
             }
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
-            app.UseMiddleware<ExceptionMiddleware>(); // apply first
-            
+            app.UseMiddleware<ExceptionMiddleware>();
+
             app.UseHttpsRedirection();
 
             app.UseAuthentication();
             app.UseAuthorization();
 
-
             app.MapControllers();
 
-            app.Run();
+            await app.RunAsync();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
         }
     }
 }
