13e789e3e594856aab6b0c4098de1ffb0fa3287b
mohamed.amin <mohamedamin1421@gmail.com>
2026-07-30 15:23:01 +0300

    ref: attendance request validation

--- AttendanceSystem.Application/Validators/Cases/Create/AttendanceRequestValidator.cs
+++ AttendanceSystem.Application/Validators/Cases/Create/AttendanceRequestValidator.cs
@@ -1,4 +1,3 @@
-using AttendanceSystem.Domain;
 using FluentValidation;
 
 namespace AttendanceSystem.Application;
@@ -8,6 +7,8 @@ public class CreateAttendanceRequestValidator
 {
     public CreateAttendanceRequestValidator()
     {
+        Include(new AttendanceRequestBaseValidator<CreateAttendanceRequestDto>());
+
         RuleFor(x => x.EmployeeId)
             .NotEmpty()
             .WithMessage("Employee is required");
@@ -15,40 +16,5 @@ public class CreateAttendanceRequestValidator
         RuleFor(x => x.RequestType)
             .IsInEnum()
             .WithMessage("Invalid request type");
-
-        RuleFor(x => x.RequestDate)
-            .NotEmpty()
-            .WithMessage("Request date is required");
-
-        RuleFor(x => x.RequestDate)
-            .Must(date => date.Date >= DateTime.Today.AddDays(-7))
-            .WithMessage("Request date cannot be too old");
-
-        RuleFor(x => x.Reason)
-            .MaximumLength(500)
-            .WithMessage("Reason cannot exceed 500 characters");
-
-        RuleFor(x => x)
-            .Must(HaveValidTimeRange)
-            .WithMessage("FromTime must be less than ToTime");
-
-        RuleFor(x => x)
-            .Must(HaveBothTimesOrNone)
-            .WithMessage("FromTime and ToTime must both have values");
-    }
-
-    private static bool HaveValidTimeRange(
-        CreateAttendanceRequestDto dto)
-    {
-        if (!dto.FromTime.HasValue || !dto.ToTime.HasValue)
-            return true;
-
-        return dto.FromTime < dto.ToTime;
-    }
-
-    private static bool HaveBothTimesOrNone(
-        CreateAttendanceRequestDto dto)
-    {
-        return dto.FromTime.HasValue == dto.ToTime.HasValue;
     }
 }
