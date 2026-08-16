8e95b2434625834c733b7db19095396fa3d5b967
mohamed.amin <mohamedamin1421@gmail.com>
2026-08-16 16:37:17 +0300

    clean up

--- AttendanceSystem.Infrastructure/Repositories/AttendanceLogRepository.cs
+++ AttendanceSystem.Infrastructure/Repositories/AttendanceLogRepository.cs
@@ -27,12 +27,6 @@ public class AttendanceLogRepository(AttendanceDbContext context)
             include: q => q.Include(x => x.Employee));
     }
 
-    public async Task<IReadOnlyList<AttendanceLog>> GetByEmployeeAndMonthAsync(Guid employeeId, int year, int month)
-        => await _context.AttendanceLogs
-            .AsNoTracking()
-            .Where(x => x.EmployeeId == employeeId && x.Date.Year == year && x.Date.Month == month)
-            .ToListAsync();
-
     public async Task<AttendanceLog?> GetByEmployeeAndDateAsync(Guid employeeId, DateTime date)
         => await _context.AttendanceLogs
             .FirstOrDefaultAsync(x => x.EmployeeId == employeeId && x.Date.Date == date.Date);
