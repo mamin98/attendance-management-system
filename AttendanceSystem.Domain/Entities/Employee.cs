94085813ea5d8d00a6a1b4194e55730c022b643c
mohamed.amin <mohamedamin1421@gmail.com>
2026-08-16 16:36:59 +0300

    feat: add relations

--- AttendanceSystem.Domain/Entities/Employee.cs
+++ AttendanceSystem.Domain/Entities/Employee.cs
@@ -16,7 +16,9 @@ public class Employee : BaseEntity
     public virtual ICollection<EmployeeLeaveBalance> LeaveBalances { get; private set; } = [];
     public virtual ICollection<LeaveRequest> LeaveRequests { get; private set; } = [];
     public virtual ICollection<AttendanceLog> AttendanceLogs { get; private set; } = [];
-
+    public virtual SalaryStructure? SalaryStructure { get; private set; }
+    public virtual ICollection<PayrollRecord> PayrollRecords { get; private set; } = [];
+    
     private Employee SetNameEnglish(string name)
     {
         NameEnglish = name.Trim();
