b4e4963819dcdcb60e70fdb80b14487d41bfc420
mohamed.amin <mohamedamin1421@gmail.com>
2026-08-16 16:18:37 +0300

    feat: implement attendance log repo

--- AttendanceSystem.Application/Interfaces/IRepositories/IAttendanceLogRepository.cs
+++ AttendanceSystem.Application/Interfaces/IRepositories/IAttendanceLogRepository.cs
@@ -4,7 +4,8 @@ namespace AttendanceSystem.Application;
 
 public interface IAttendanceLogRepository : IGenericRepository<AttendanceLog>
 {
+    Task<PagedResult<AttendanceLog>> GetAllWithPaginationAsync(AttendanceLogSearchDto search);
     Task<AttendanceLog?> GetByEmployeeAndDateAsync(Guid employeeId, DateTime date);
     Task<IReadOnlyList<AttendanceLog>> GetByEmployeesAndMonthAsync(List<Guid> employeeId, int year, int month);
-    Task AddRangeAsync(IEnumerable<AttendanceLog> entities);
+    Task AddRangeAsync(IEnumerable<AttendanceLog> entities);    
 }
--- AttendanceSystem.Infrastructure/Repositories/AttendanceLogRepository.cs
+++ AttendanceSystem.Infrastructure/Repositories/AttendanceLogRepository.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using AttendanceSystem.Application;
 using AttendanceSystem.Domain;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,31 @@ namespace AttendanceSystem.Infrastructure;
 public class AttendanceLogRepository(AttendanceDbContext context)
     : GenericRepository<AttendanceLog>(context), IAttendanceLogRepository
 {
+    public async Task<PagedResult<AttendanceLog>> GetAllWithPaginationAsync(AttendanceLogSearchDto search)
+    {
+        Expression<Func<AttendanceLog, bool>>? filter = null;
+
+        if (search.EmployeeId.HasValue || search.FromDate.HasValue || search.ToDate.HasValue)
+        {
+            filter = x =>
+                (!search.EmployeeId.HasValue || x.EmployeeId == search.EmployeeId)
+                && (!search.FromDate.HasValue || x.Date >= search.FromDate)
+                && (!search.ToDate.HasValue || x.Date <= search.ToDate);
+        }
+
+        return await GetAllWithPaginationAsync(
+            search.Page,
+            search.PageSize,
+            filter: filter,
+            include: q => q.Include(x => x.Employee));
+    }
+
+    public async Task<IReadOnlyList<AttendanceLog>> GetByEmployeeAndMonthAsync(Guid employeeId, int year, int month)
+        => await _context.AttendanceLogs
+            .AsNoTracking()
+            .Where(x => x.EmployeeId == employeeId && x.Date.Year == year && x.Date.Month == month)
+            .ToListAsync();
+
     public async Task<AttendanceLog?> GetByEmployeeAndDateAsync(Guid employeeId, DateTime date)
         => await _context.AttendanceLogs
             .FirstOrDefaultAsync(x => x.EmployeeId == employeeId && x.Date.Date == date.Date);
@@ -19,4 +45,10 @@ public class AttendanceLogRepository(AttendanceDbContext context)
 
     public async Task AddRangeAsync(IEnumerable<AttendanceLog> entities)
         => await _context.AttendanceLogs.AddRangeAsync(entities);
+    
+    public override async Task<AttendanceLog?> GetByIdAsync(Guid id)
+        => await _context.AttendanceLogs
+            .Include(x => x.Employee)
+            .FirstOrDefaultAsync(x => x.Id == id);
+
 }
